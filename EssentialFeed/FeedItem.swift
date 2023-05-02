9713c97dc74f289f71f67c89e4a765e641e231cc
Vishal Deep Kanojia <vishal.deep@live.com>
2023-05-02 12:34:01 +0530

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vishal on 02/05/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
