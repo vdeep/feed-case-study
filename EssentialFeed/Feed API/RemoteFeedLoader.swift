00c2bd4ecbc6d66e61c7c03ad3c396fcc6b1de29
Vishal Deep Kanojia <vishal.deep@live.com>
2023-05-05 12:58:06 +0530

    Add RemoteFeedLoader Result type

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -14,6 +14,8 @@ public protocol HTTPClient {
 }
 
 public final class RemoteFeedLoader {
+    public typealias Result = Swift.Result<[FeedItem], Error>
+
     private let url: URL
     private let client: HTTPClient
 
@@ -27,13 +29,13 @@ public final class RemoteFeedLoader {
         self.client = client
     }
 
-    public func load(completion: @escaping (Error) -> Void) {
+    public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { result in
             switch result {
             case .success:
-                completion(.invalidData)
+                completion(.failure(.invalidData))
             case .failure:
-                completion(.connectivity)
+                completion(.failure(.connectivity))
             }
         }
     }
