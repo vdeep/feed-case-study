591445902263195b1b2cc58e46e2c4dd143905a8
Vishal Deep Kanojia <vishal.deep@live.com>
2023-05-07 19:48:12 +0530

    Made FeedLoader and LoadFeedResult types public

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-typealias LoadFeedResult = Result<[FeedItem], Error>
+public typealias LoadFeedResult = Result<[FeedItem], Error>
 
-protocol FeedLoader {
+public protocol FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void)
 }
