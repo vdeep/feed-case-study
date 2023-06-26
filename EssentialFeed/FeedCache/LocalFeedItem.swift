0563b5938dde12cb40571614d5618d6695cb8ad1
Vishal Deep Kanojia <vishal.deep@live.com>
2023-06-26 22:02:46 +0530

    Move `LocalFeedItem` to its own file

---
+++ EssentialFeed/FeedCache/LocalFeedItem.swift
@@ -0,0 +1,27 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vishal on 26/06/23.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(
+        id: UUID,
+        description: String? = nil,
+        location: String? = nil,
+        imageURL: URL
+    ) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
