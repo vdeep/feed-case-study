671aa1fc63de87cd232adcccbd58676dcf3d8218
Vishal Deep Kanojia <vishal.deep@live.com>
2023-07-04 14:47:47 +0530

    Make `CodableFeedStore` conform to `FeedStore`

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class CodableFeedStore {
+class CodableFeedStore: FeedStore {
     private struct Cache: Codable {
         let feed: [CodableFeedImage]
         let timestamp: Date
@@ -50,7 +50,7 @@ class CodableFeedStore {
     func insert(
         _ feed: [LocalFeedImage],
         timestamp: Date,
-        completion: @escaping FeedStore.InsertionCompletion
+        completion: @escaping InsertionCompletion
     ) {
         do {
             let encoder = JSONEncoder()
@@ -66,7 +66,7 @@ class CodableFeedStore {
         }
     }
     
-    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+    func retrieve(completion: @escaping RetrievalCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
             return completion(.empty)
         }
@@ -80,7 +80,7 @@ class CodableFeedStore {
         }
     }
     
-    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         guard FileManager.default.fileExists(atPath: storeURL.path) else {
             return completion(nil)
         }
