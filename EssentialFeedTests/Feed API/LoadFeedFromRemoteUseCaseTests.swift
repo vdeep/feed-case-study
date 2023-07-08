8d494ee224a8712a3e382ad26d9ccd07b4841164
Vishal Deep Kanojia <vishal.deep@live.com>
2023-07-08 15:09:37 +0530

    Nest `HTTPClientResult` into the `HTTPClient` protocol as `HTTPClient.Result` because they're closely related

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -181,12 +181,12 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     }
 
     class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         var requestedURLs: [URL] {
             messages.map { $0.url }
         }
 
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
 
