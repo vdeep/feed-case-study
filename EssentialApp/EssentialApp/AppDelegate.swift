dc08b9a9b2557e88ae5f77f6a3a6463202f0b69a
Vishal Deep Kanojia <vishal.deep@live.com>
2023-08-04 20:02:14 +0530

    Removed unused `didFinishLaunchingWithOptions` method from `AppDelegate`

--- EssentialApp/EssentialApp/AppDelegate.swift
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -9,16 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
+    
     func application(
         _ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
