96a112fcadd2027c09e620db126315fce85d85ab
Vishal Deep Kanojia <vishal.deep@live.com>
2023-08-20 20:32:20 +0530

    Add ImageComments title

---
+++ EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,19 @@
+//
+//  ImageCommentsPresenter.swift
+//  EssentialFeed
+//
+//  Created by Vishal on 20/08/23.
+//
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        NSLocalizedString(
+            "IMAGE_COMMENTS_VIEW_TITLE",
+            tableName: "ImageComments",
+            bundle: Bundle(for: Self.self),
+            comment: "Title for the image comments view"
+        )
+    }
+}
