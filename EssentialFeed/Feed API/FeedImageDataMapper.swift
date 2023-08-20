5aef65e9faa6efdd2cfb573cf752362a06fb85c7
Vishal Deep Kanojia <vishal.deep@live.com>
2023-08-20 12:00:52 +0530

    Assert `FeedImageDataMapper` throws error for non 200 HTTP response

---
+++ EssentialFeed/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeed
+//
+//  Created by Vishal on 20/08/23.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK else {
+            throw Error.invalidData
+        }
+
+        return Data()
+    }
+}
---
+++ EssentialFeedTests/Feed API/FeedImageDataMapperTests.swift
@@ -0,0 +1,25 @@
+//
+//  FeedImageDataMapperTests.swift
+//  EssentialFeedTests
+//
+//  Created by Vishal on 20/08/23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedImageDataMapperTests: XCTestCase {
+
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+        let samples = [199, 201, 300, 400, 500]
+
+        try samples.forEach { code in
+            XCTAssertThrowsError(try FeedImageDataMapper
+                .map(
+                    anyData(),
+                    from: HTTPURLResponse(statusCode: code)
+                )
+            )
+        }
+    }
+}
