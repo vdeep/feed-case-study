5f316a312f4e84e2f3bcd9315d5d3203085318df
Vishal Deep Kanojia <vishal.deep@live.com>
2023-08-26 20:14:23 +0530

    Move localisation helpers to shared scope

--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -30,3 +30,19 @@ func uniqueFeed() -> [FeedImage] {
         )
     ]
 }
+
+class DummyView: ResourceView {
+    func display(_ viewModel: Any) {}
+}
+
+var loadError: String {
+    LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    FeedPresenter.title
+}
+
+var commentsTitle: String {
+    ImageCommentsPresenter.title
+}
