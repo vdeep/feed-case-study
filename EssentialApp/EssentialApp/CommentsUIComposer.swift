6342bb058608df30fd388ac253ebd0b6f5f72d28
Vishal Deep Kanojia <vishal.deep@live.com>
2023-08-26 17:47:38 +0530

    Duplicate `FeedUICompoer` as `CommentsUIComposer`

---
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -0,0 +1,45 @@
+//
+//  CommentsUIComposer.swift
+//  EssentialApp
+//
+//  Created by Vishal on 26/08/23.
+//
+
+import Combine
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+public final class CommentsUIComposer {
+    private init() {}
+
+    public static func commentsComposedWith(
+        commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>
+    ) -> ListViewController {
+        let presentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>(
+            loader: commentsLoader
+        )
+
+        let feedController = makeFeedViewController(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
+
+        presentationAdapter.presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: { _ in Empty<Data, Error>().eraseToAnyPublisher() }
+            ),
+            loadingView: WeakRefVirtualProxy(feedController),
+            errorView: WeakRefVirtualProxy(feedController),
+            mapper: FeedPresenter.map
+        )
+        return feedController
+    }
+
+    private static func makeFeedViewController(title: String) -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+        feedController.title = FeedPresenter.title
+        return feedController
+    }
+}
