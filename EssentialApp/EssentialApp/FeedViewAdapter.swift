4f4e1423d1dd1477363af43061ef58e878fc7d7a
Vishal Deep Kanojia <vishal.deep@live.com>
2023-09-01 22:25:57 +0530

    Prevent load more action while loading more

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -13,6 +13,9 @@ final class FeedViewAdapter: ResourceView {
     private weak var controller: ListViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
     private let selection: (FeedImage) -> Void
+
+    private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>
+    private typealias LoadMorePresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>, FeedViewAdapter>
     
     init(
         controller: ListViewController,
@@ -26,7 +29,7 @@ final class FeedViewAdapter: ResourceView {
     
     func display(_ viewModel: Paginated<FeedImage>) {
         let feed: [CellController] = viewModel.items.map { model in
-            let adapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>(
+            let adapter = ImageDataPresentationAdapter(
                 loader: { [imageLoader] in
                     imageLoader(model.url)
                 }
@@ -55,9 +58,20 @@ final class FeedViewAdapter: ResourceView {
             return CellController(id: model, view)
         }
 
-        let loadMore = LoadMoreCellController {
-            viewModel.loadMore?({ _ in })
+        guard let loadMorePublisher = viewModel.loadMorePublisher else {
+            controller?.display(feed)
+            return
         }
+
+        let loadMoreAdapter = LoadMorePresentationAdapter(loader: loadMorePublisher)
+        let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
+
+        loadMoreAdapter.presenter = LoadResourcePresenter(
+            resourceView: self,
+            loadingView: WeakRefVirtualProxy(loadMore),
+            errorView: WeakRefVirtualProxy(loadMore),
+            mapper: { $0 }
+        )
         let loadMoreSection = [CellController(id: UUID(), loadMore)]
 
         controller?.display(feed, loadMoreSection)
